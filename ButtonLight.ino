81e8d2834cd397b9be3bdf5ff9e1af5443c94565
null <davidking9753@gmail.com>
2017-06-26 10:39:13 +0200

    V0.1 Shifts over 4 states. V0.2 Place the booleans in memory.

--- ButtonLight.ino
+++ ButtonLight.ino
@@ -1,11 +1,19 @@
 int buttonPin = 1;
 int lightPin = 2;
 int buttonState = 0;
-long buttonTimer;
+int minPin = 2;
+int maxPin = 5;
 
-boolean lightOn = false;
+long buttonTimer;
+long timeout = 5000;
+
+boolean lightOn = true;
 boolean buttonActive = false;
 boolean longPressActive = false;
+boolean light1 = false;
+boolean light2 = false;
+boolean light3 = false;
+boolean light4 = false;
 
 const long longPressTime = 250;
 
@@ -14,25 +22,38 @@ const long longPressTime = 250;
 void setup(){
   Serial.begin(9600);
   pinMode(buttonPin,INPUT);
-  pinMode(lightPin,OUTPUT);
+  //Less than max
+  for(int i = minPin; i<=maxPin; i++){
+  pinMode(i,OUTPUT);
+  }
 }
 
 void loop(){
-  buttonState = digitalRead(buttonPin);
+  if(lightOn){
+    turnLightOn();
+  }
+  checkBtn(timeout);
+}
   
-  if(buttonState == HIGH){
-    Serial.print("im high+ ");
-    Serial.println(buttonTimer);
-    //Button pressed
-    if(!longPressActive){}
+  void checkBtn(long duration){
+  unsigned long start = millis();
+
+  while (millis() - start <= duration) {    
+    buttonState = digitalRead(buttonPin);
+  
+    if(buttonState == HIGH){
+      Serial.print("im high+ ");
+      Serial.println(buttonTimer);
+      //Button pressed
+      if(!longPressActive){}
     
-    if(buttonActive == false){
-     buttonActive = true;
-     buttonTimer = millis();
-    }if((millis() - buttonTimer > longPressTime) && (longPressActive == false)){
-    Serial.println("Longpress ACTIVATED!");
-    longPressActive = true;
-    digitalWrite(lightPin, HIGH);
+        if(buttonActive == false){
+           buttonActive = true;
+           buttonTimer = millis();
+        }if((millis() - buttonTimer > longPressTime) && (longPressActive == false)){
+          Serial.println("Longpress ACTIVATED!");
+          longPressActive = true;
+          digitalWrite(lightPin, HIGH);
   }
   
 }else{
@@ -52,4 +73,37 @@ void loop(){
        longPressActive = false;
       }
     }
+   }
   }
+  
+  void turnLightOn(){
+  if(light1 ==false && light2 == false && light3 == false && (light4 ==false || light4 == true)){
+    for(int i = minPin; i<=maxPin; i++){
+      digitalWrite(i,LOW);
+    }  
+    digitalWrite(2,HIGH);
+    light1 = true;  
+  }else if (light1 == true){
+  for(int i = minPin; i<=maxPin; i++){
+      digitalWrite(i,LOW);
+    }
+    digitalWrite(3,HIGH);
+    light1 = false;
+    light2 = true;
+  }else if (light2 == true){
+    for(int i = minPin; i<=maxPinmaxPin; i++){
+      digitalWrite(i,LOW);
+    }
+    digitalWrite(4,HIGH);
+    light2 = false;
+    light3 = true;
+  }else if (light3 == true){
+  for(int i = minPin; i<=maxPin; i++){
+      digitalWrite(i,LOW);
+    }
+    digitalWrite(5,HIGH);
+    light3 = false;
+    light4 = true;
+  }
+}
+  void lightOff(){}
